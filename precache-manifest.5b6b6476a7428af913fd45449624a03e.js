60ddbdc61c13c165d5cbe330a71721781894e9e8
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-08-03 14:44:39 -0400

    Updates

---
+++ precache-manifest.5b6b6476a7428af913fd45449624a03e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2787ca51d7171c0ed35e91cd10ae343",
+    "url": "/portfolio-r/index.html"
+  },
+  {
+    "revision": "3a7fc9e072519e47728f",
+    "url": "/portfolio-r/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ef407504b1dc42c7331",
+    "url": "/portfolio-r/static/js/2.6e3aeba8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-r/static/js/2.6e3aeba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7fc9e072519e47728f",
+    "url": "/portfolio-r/static/js/main.72d4cc6e.chunk.js"
+  },
+  {
+    "revision": "25fc8348085800a59277",
+    "url": "/portfolio-r/static/js/runtime-main.9d7fb723.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-r/static/media/logo.5d5d9eef.svg"
+  }
+]);
